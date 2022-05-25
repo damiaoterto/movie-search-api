679e2144520a30aaf10ee5e241d1dcee1fc3bb40
Francisco Damião Terto <hi@damiaojunior.me>
2022-05-24 22:56:38 -0300

    create movies test

--- test/integration/movies.spec.js
+++ test/integration/movies.spec.js
@@ -1,11 +1,19 @@
 import request from 'supertest';
 import { server } from '../../src/server';
 
-describe('Movies', () => {
+describe('Movies Search' , () => {
+  it('should return not found message', async () => {
+    const response = await request(server).get('/');
+
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe('Not Found');
+  });
+
   it('should return a movie list by search string', async () => {
     const response = await request(server).get('/movies?search=star');
 
     expect(response.status).toBe(200);
+    expect(Array.isArray(response.body.movies)).toBe(true);
     expect(response.body).toHaveProperty('movies');
   });
 });
